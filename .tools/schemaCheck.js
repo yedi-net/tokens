3e3684d7cfebd219641f79b9fcd5e11e72bdab01
7bp <bp@bp.la>
2023-10-10 23:02:11 +0000

    Add DEEPSPACE (DPS) on ETH

--- .tools/schemaCheck.js
+++ .tools/schemaCheck.js
@@ -8,12 +8,12 @@ const path = require('path')
 const resolve = (_path) => path.resolve(__dirname, _path)
 
 const chains = ["bnb", "eth"]
-const folders = chains.flatMap(chain => fs.readdirSync(resolve(`./${chain}`)).map(folder => `${chain}/${folder}`))
-const files = folders.flatMap(folder => fs.readdirSync(resolve(`./${folder}`)).map(file => `${folder}/${file}`))
+const folders = chains.flatMap(chain => fs.readdirSync(resolve(`../${chain}`)).map(folder => `${chain}/${folder}`))
+const files = folders.flatMap(folder => fs.readdirSync(resolve(`../${folder}`)).map(file => `${folder}/${file}`))
 
 const filesWithErrors = []
 for (const file of files) {
-  const fileLocation = resolve(`./${file}`)
+  const fileLocation = resolve(`../${file}`)
   if (fileLocation.endsWith("/data.json")) {
     const fileData = fs.readFileSync(fileLocation, "utf8")
     const fileDataJson = JSON.parse(fileData)
